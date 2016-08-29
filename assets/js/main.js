df2dfa7dc4ccb3da9d4f6be63de64a76094320f2
Matej Ramuta <matej.ramuta@gmail.com>
2016-08-29 19:03:08 +0200

    Encryption added

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,4 +20,41 @@ $(document).ready(function() {
     });
 
     $('[data-toggle="popover"]').popover({placement: 'top'});
+
+    /* New code by Matej Ramuta */
+    var encryptionButton = $("#encryption-button");
+
+    encryptionButton.click(function(){
+        var encryptionPlainText = $("#encryption-plain-text");
+        var encryptionEncryptedText = $("#encryption-encrypted-text");
+        var encryptionReceiversPublicKey = $("#encryption-receivers-public-key");
+
+        // import receiver's public key
+        var matt = kbpgp.KeyManager.import_from_armored_pgp({
+          armored: encryptionReceiversPublicKey.val()
+        }, function(err, matt) {
+          if (!err) {
+            console.log("matt's public key is loaded");
+            console.log(matt);
+
+            // encrypt the message
+            var params = {
+              msg: encryptionPlainText.val(),
+              encrypt_for: matt
+            };
+
+            kbpgp.box(params, function(err, result_string, result_buffer) {
+              console.log(err, result_string, result_buffer);
+              encryptionEncryptedText.val(result_string);
+            });
+
+          } else {
+            console.log("Error!");
+          }
+        });
+
+        console.log(matt);
+
+
+    });
 });
