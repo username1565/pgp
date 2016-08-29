bd8b566833123a1abb41f74880a76ba553c0e254
Matej Ramuta <matej.ramuta@gmail.com>
2016-08-29 21:05:40 +0200

    decryption implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,8 @@ $(document).ready(function() {
     $('[data-toggle="popover"]').popover({placement: 'top'});
 
     /* New code by Matej Ramuta */
+
+    // ENCRYPTION
     var encryptionButton = $("#encryption-button");
 
     encryptionButton.click(function(){
@@ -30,17 +32,17 @@ $(document).ready(function() {
         var encryptionReceiversPublicKey = $("#encryption-receivers-public-key");
 
         // import receiver's public key
-        var matt = kbpgp.KeyManager.import_from_armored_pgp({
+        var receiver = kbpgp.KeyManager.import_from_armored_pgp({
           armored: encryptionReceiversPublicKey.val()
-        }, function(err, matt) {
+        }, function(err, receiver) {
           if (!err) {
-            console.log("matt's public key is loaded");
-            console.log(matt);
+            console.log("receiver's public key is loaded");
+            console.log(receiver);
 
             // encrypt the message
             var params = {
               msg: encryptionPlainText.val(),
-              encrypt_for: matt
+              encrypt_for: receiver
             };
 
             kbpgp.box(params, function(err, result_string, result_buffer) {
@@ -52,9 +54,64 @@ $(document).ready(function() {
             console.log("Error!");
           }
         });
+    });
 
-        console.log(matt);
+    // DECRYPTION
+    var decryptionButton = $("#decryption-button");
 
+    decryptionButton.click(function(){
+        var decryptionEncryptedText = $("#decryption-encrypted-text");
+        var decryptionDecryptedText = $("#decryption-decrypted-text");
+        var decryptionPrivateKey = $("#decryption-private-key");
+        var decryptionPassphrase = $("#decryption-passphrase");
 
+        console.log(decryptionEncryptedText);
+
+        // import receiver's public key
+        var currUser = kbpgp.KeyManager.import_from_armored_pgp({
+          armored: decryptionPrivateKey.val()
+        }, function(err, currUser) {
+          if (!err) {
+            if (currUser.is_pgp_locked()) {
+              currUser.unlock_pgp({
+                passphrase: decryptionPassphrase.val()
+              }, function(err) {
+                if (!err) {
+                  console.log("Loaded private key with passphrase");
+
+                  // add KeyRing
+                  var ring = new kbpgp.keyring.KeyRing;
+                  ring.add_key_manager(currUser);
+
+                  kbpgp.unbox({keyfetch: ring, armored: decryptionEncryptedText.val()}, function(err, literals) {
+                    if (err != null) {
+                      return console.log("Problem: " + err);
+                    } else {
+                      var decryptedText = literals[0].toString();
+                      console.log("decrypted message: " + decryptedText);
+
+                      decryptionDecryptedText.val(decryptedText);
+
+                      var ds = km = null;
+                      ds = literals[0].get_data_signer();
+                      if (ds) { km = ds.get_key_manager(); }
+                      if (km) {
+                        console.log("Signed by PGP fingerprint");
+                        console.log(km.get_pgp_fingerprint().toString('hex'));
+                      }
+                    }
+                  });
+
+                } else {
+                  console.log("Error in decryption unlock pgp");
+                }
+              });
+            } else {
+              console.log("Loaded private key w/o passphrase");
+            }
+          } else {
+            console.log("Error in decryption import");
+          }
+        });
     });
 });
